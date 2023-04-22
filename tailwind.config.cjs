a07538018f26c4c938eccdfdda0f0854ce2a9274
David Buckley <runalldayandnight@gmail.com>
2023-04-21 21:54:29 -0600

    headings and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        serif: [...defaultTheme.fontFamily.serif],
+        mono: [...defaultTheme.fontFamily.mono],
+        heading: ["Inter", ...defaultTheme.fontFamily.sans],
+        body: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        ...defaultTheme.colors,
       },
     },
   },
